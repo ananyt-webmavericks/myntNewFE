be67a11abfb411ff0f0b49250bc2ee62304c846b
Aditya Singh <aditya@thesoftcoders.com>
2023-01-23 19:25:52 +0530

    linked url to btns

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,10 @@
 import { Button } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import '../css/navbar.css';
 import logo from '../images/mynt1.png'
 const Navbar = () => {
-
+    const navigate = useNavigate()
     return (
         <nav class="navbar">
             <div class="navbar-container container">
@@ -14,12 +15,12 @@ const Navbar = () => {
                     <span class="line line3"></span>
                 </div>
                 <ul class="menu-items">
-                    <li><a href="#">Home</a></li>
+                    <li><a href="/">Home</a></li>
                     <li><a href="#">Deals</a></li>
                     <li><a href="#">Raise</a></li>
                     <li><a href="#">MintUniversity</a></li>
                     <li><button className="login-btn">Login</button></li>
-                    <li><button className="get-started-btn">Get Started</button></li>
+                    <li><button className="get-started-btn" onClick={()=> navigate('/get-started')} >Get Started</button></li>
                 </ul>
                 <img class="logo-web" src={logo}></img>
             </div>
